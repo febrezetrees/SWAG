24a448a8382aa61440649e745fcd5f9b69ba181a
RWP <ryanpieszko@gmail.com>
2022-07-24 15:51:14 +1000

    function role assigner

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -6,49 +6,44 @@
 
 //The function can use the user's information to determine which roles to assign to the user. It must return an HTTP 200 response with a JSON body containing a list of custom role names to assign to the user.
 
-/* Example payload
-{
-  "identityProvider": "aad",
-  "userId": "72137ad3-ae00-42b5-8d54-aacb38576d76",
-  "userDetails": "ellen@contoso.com",
-  "claims": [
-      {
-          "typ": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
-          "val": "ellen@contoso.com"
-      },
-      {
-          "typ": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname",
-          "val": "Contoso"
-      },
-      {
-          "typ": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname",
-          "val": "Ellen"
-      },
-      {
-          "typ": "name",
-          "val": "Ellen Contoso"
-      },
-      {
-          "typ": "http://schemas.microsoft.com/identity/claims/objectidentifier",
-          "val": "7da753ff-1c8e-4b5e-affe-d89e5a57fe2f"
-      },
-      {
-          "typ": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
-          "val": "72137ad3-ae00-42b5-8d54-aacb38576d76"
-      },
-      {
-          "typ": "http://schemas.microsoft.com/identity/claims/tenantid",
-          "val": "3856f5f5-4bae-464a-9044-b72dc2dcde26"
-      },
-      {
-          "typ": "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
-          "val": "ellen@contoso.com"
-      },
-      {
-          "typ": "ver",
-          "val": "1.0"
-      }
-  ],
-  "accessToken": "eyJ0eXAiOiJKV..."
+const fetch = require('node-fetch').default;
+
+// add role names to this object to map them to group ids in your AAD tenant
+const roleGroupMappings = {
+    'admin': '21a96550-aa02-486e-9297-e6e51b6398fc',
+    'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
+};
+
+module.exports = async function (context, req) {
+    const user = req.body || {};
+    const roles = [];
+    
+    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+        if (await isUserInGroup(groupId, user.accessToken)) {
+            roles.push(role);
+        }
+    }
+
+    context.res.json({
+        roles
+    });
 }
-*/
+
+async function isUserInGroup(groupId, bearerToken) {
+    const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
+    url.searchParams.append('$filter', `id eq '${groupId}'`);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const graphResponse = await response.json();
+    const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
+    return matchingGroups.length > 0;
+}
